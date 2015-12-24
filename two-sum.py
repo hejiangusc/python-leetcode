288a6af76062d9f02e4431aa338fe9392e6d7645
hejiangusc <jianghewade@gmail.com>
2015-12-24 09:10:58 -0600

    one more test case on No1 two sum

--- two-sum.py
+++ two-sum.py
@@ -8,9 +8,14 @@ class Solution:
     		dict[nums[i]] = i
 
 test1 = Solution()
-num_test = [1,2,6,22,34]
-print test1.twoSum(num_test, 8)
+num_test1 = [1,2,6,22,34]
+print test1.twoSum(num_test1, 8)
 
 test2 = Solution()
 num_test2 = [2,7,11,15]
 print test2.twoSum(num_test2, 9)
+
+test3 = Solution()
+num_test3 = [1,3,5,6,8,23,45,56,34,22,46,102,114,7]
+print test3.twoSum(num_test3, 78)
+
